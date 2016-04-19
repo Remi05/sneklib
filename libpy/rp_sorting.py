fcae79fe9f74a3fb3452d218cc0cb53d76a19650
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2016-04-19 15:40:20 -0400

    Added QuickSort and creation of random lists.

--- libpy/rp_sorting.py
+++ libpy/rp_sorting.py
@@ -3,7 +3,18 @@
 #Desc.:  Module containing various sorting functions.
 
 
+import random
+
+
 #TODO: Consider returning a new list instead of modifying the list passed as an argument.
+
+
+#Create a list (vector) of the given size and fills it with random numbers between min_val and max_val (both inclusive).
+def createRandomVector(size, min_val, max_val):
+    vector = [0]*size
+    for i in range(0, size):
+        vector[i] = random.randrange(min_val, max_val+1, 1)
+    return vector
 
 
 #Swaps two elements in a list (vector).
@@ -43,6 +54,21 @@ def insertionSort(vector, reverse = False):
             vector[j] = vector[j-1]
             j -= 1
         vector[j] = tmp
+
+
+#Sorts the elements in a list (vector) within the given range (start and end are inclusive)
+#using Insertion sort, can sort in reverse order as well.
+def insertionSort(vector, start, end, reverse = False):
+    if(start < end):
+        for i in range(start+1, end+1):
+            tmp = vector[i]
+            j = i
+            while j > start and ((tmp < vector[j-1] and not reverse) or\
+                                 (tmp > vector[j-1] and reverse)):
+                vector[j] = vector[j-1]
+                j -= 1
+            vector[j] = tmp
+
 
 
 #Merges the vector to be sorted and the temporary vector in the specified range (used for sorting using mergeSort()).
@@ -89,8 +115,57 @@ def _mergeSort(vector, tmp, left, right):
 def mergeSort(vector):
     tmp = [0]*len(vector)
     _mergeSort(vector, tmp, 0, len(vector)-1)
-    
-    
+
+
+
+DEFAULT_QUICKSORT_CUTOFF = 3;
+
+#Gets the median of the elements of the vector at left, right and (left + right) // 2 to use as pivot for QuickSort. 
+#Also rearranges these elements for the next iteration of QuickSort (called in _quickSort()).
+def _median3(vector, left, right):
+    center = (left + right) // 2
+
+    if(vector[center] < vector[left]):
+        swap(vector, center, left)
+    if(vector[right] < vector[left]):
+        swap(vector, right, left)
+    if(vector[right] < vector[center]):
+        swap(vector, right, center)
+
+    swap(vector, center, right-1)
+    return vector[right-1]
+
+
+#Recursive function used to sort sections of a vector (used for sorting using quickSort()).
+def _quickSort(vector, left, right, cutoff):
+    if (left + cutoff) <= right:
+        pivot = _median3(vector, left, right)
+        i = left
+        j = right - 1
+        while True:
+            i+=1
+            while vector[i] < pivot:
+                i+=1
+            j-=1
+            while vector[j] > pivot:
+                j-=1
+            if i < j:
+                swap(vector, i, j)
+            else:
+                break
+        swap(vector, i, right-1)
+        _quickSort(vector, left, i-1, cutoff)
+        _quickSort(vector, i+1, right, cutoff)
+    else:
+        insertionSort(vector, left, right)
+
+
+#Sorts the elements in a list (vector) using QuickSort.
+def quickSort(vector, cutoff = DEFAULT_QUICKSORT_CUTOFF):
+    _quickSort(vector, 0, len(vector)-1, cutoff)
+
+
+
 #Sorts the elements in a list (vector) using Counting sort (use only when the
 #range of values is significantly smaller than the number of values).
 def countingSort(vector):
@@ -131,3 +206,10 @@ def countingSortWithValues(vector):
 #(useful when using countingSortWithValues() to keep only the values after sorting).
 def stripKeys(vector):
     return [tup[1] for tup in vector]
+
+
+
+#Test
+vector = createRandomVector(15, 1, 100)
+quickSort(vector)
+print(vector)
