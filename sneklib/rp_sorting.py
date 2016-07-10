09b74f3beb17188474b1288856da4790cd2e5a9e
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2016-07-09 20:09:28 -0400

    Added Odd-even sort and refactored.

--- sneklib/rp_sorting.py
+++ sneklib/rp_sorting.py
@@ -6,59 +6,78 @@
 import random
 
 
-#Creates a list (vector) of the given size and fills it with random numbers between min_val and max_val (both inclusive).
-def createRandomVector(size, min_val, max_val):
-    vector = [0]*size
+#Creates a list of the given size and fills it with random numbers between min_val and max_val (both inclusive).
+def createRandomList(size, min_val, max_val):
+    lst = [0]*size
     for i in range(0, size):
-        vector[i] = random.randrange(min_val, max_val+1, 1)
-    return vector
+        lst[i] = random.randrange(min_val, max_val+1, 1)
+    return lst
 
 
-#Swaps two elements in a list (vector).
-def swap(vector, i1, i2):
-    tmp = vector[i1]
-    vector[i1] = vector[i2]
-    vector[i2] = tmp
+#Swaps two elements in a list .
+def swap(lst, i1, i2):
+    lst[i1], lst[i2] = lst[i2], lst[i1]
 
 
-#Sorts the elements in a list (vector) using Selection sort, can sort in reverse order as well.
-def selectionSort(vector, reverse = False):
-    for i in range(0, len(vector)-1):
+#Sorts the elements in a list using Selection sort, can sort in reverse order as well.
+def selectionSort(lst, reverse = False):
+    for i in range(0, len(lst)-1):
         min = i
-        for j in range(i+1, len(vector)):
-            if (not reverse and vector[j] < vector[min]) or\
-               (    reverse and vector[j] > vector[min]):
+        for j in range(i+1, len(lst)):
+            if (not reverse and lst[j] < lst[min]) or\
+               (    reverse and lst[j] > lst[min]):
                 min = j
-        swap(vector, i, min)
+        swap(lst, i, min)
 
 
-#Sorts the elements in a list (vector) using Bubble sort, can sort in reverse order as well.
-def bubbleSort(vector, reverse = False):
-    v_length = len(vector)
+#Sorts the elements in a list using Bubble sort, can sort in reverse order as well.
+def bubbleSort(lst, reverse = False):
+    v_length = len(lst)
     for i in range(0, v_length-1):
         for j in range(v_length-1, i, -1):
-            if (not reverse and vector[j] < vector[j-1]) or\
-               (    reverse and vector[j] > vector[j-1]):
-                swap(vector, j, j-1)
+            if (not reverse and lst[j] < lst[j-1]) or\
+               (    reverse and lst[j] > lst[j-1]):
+                swap(lst, j, j-1)
 
 
-#Sorts the elements in a list (vector) using an improved version of Bubble sort, can sort in reverse order as well.
-def improvedBubbleSort(vector, reverse = False):
-    end = len(vector)
+#Sorts the elements in a list using an improved version of Bubble sort, can sort in reverse order as well.
+def improvedBubbleSort(lst, reverse = False):
+    end = len(lst)
     swapped = True
     while swapped:
         swapped = False
         for i in range(1, end):
-            if (not reverse and vector[i] < vector[i-1]) or\
-               (    reverse and vector[i] > vector[i-1]):
-                swap(vector, i, i-1)
+            if (not reverse and lst[i] < lst[i-1]) or\
+               (    reverse and lst[i] > lst[i-1]):
+                swap(lst, i, i-1)
                 swapped = True
         end -= 1
 
 
-#Sorts the elements in a list (vector) using Comb sort, can sort in reverse order as well.
-def combSort(vector, reverse = False):
-    v_length = len(vector)
+#Function used in oddEvenSort() to perform either an odd-even or an even-odd iteration.
+#Prevents code duplication and conditional statement evaluation at each iteration.
+def _oddEvenIter(lst, start, end, reverse = False):
+    swapped = False
+    for i in range(start, end, 2):
+        if (not reverse and lst[i] < lst[i-1]) or\
+           (    reverse and lst[i] > lst[i-1]):
+            swap(lst, i, i-1)
+            swapped = True  
+    return swapped
+
+
+#Sorts the elements in a list using Odd-even sort, can sort in reverse order as well.
+def oddEvenSort(lst, reverse = False):
+    length = len(lst)
+    swapped = True
+    while swapped:
+        swapped  = _oddEvenIter(lst, 1, length, reverse) #Odd-even iteration
+        swapped |= _oddEvenIter(lst, 2, length, reverse) #Even-odd iteration
+
+
+#Sorts the elements in a list using Comb sort, can sort in reverse order as well.
+def combSort(lst, reverse = False):
+    v_length = len(lst)
     shrink_factor = 1.3
     gap = v_length
     swapped = True
@@ -69,201 +88,201 @@ def combSort(vector, reverse = False):
         swapped = False
         i = 0
         while (i + gap) < v_length:
-            if (not reverse and vector[i] > vector[i+gap]) or\
-               (    reverse and vector[i] < vector[i+gap]):
-                swap(vector, i, i + gap)
+            if (not reverse and lst[i] > lst[i+gap]) or\
+               (    reverse and lst[i] < lst[i+gap]):
+                swap(lst, i, i + gap)
                 swapped = True
             i += 1
 
 
-#Sorts the elements in a list (vector) using Cocktail sort, can sort in reverse order as well.
-def cocktailSort(vector, reverse = False):  
+#Sorts the elements in a list using Cocktail sort, can sort in reverse order as well.
+def cocktailSort(lst, reverse = False):  
     left_end  = 0
-    right_end = len(vector) - 1
+    right_end = len(lst) - 1
     swapped   = True
 
     while swapped:
         for i in range(left_end, right_end):
-            if (not reverse and vector[i] > vector[i+1]) or\
-               (    reverse and vector[i] < vector[i+1]):
-                swap(vector, i, i+1)
+            if (not reverse and lst[i] > lst[i+1]) or\
+               (    reverse and lst[i] < lst[i+1]):
+                swap(lst, i, i+1)
                 swapped = True
             else:
                 swapped = False
         right_end -= 1
 
         for j in range(right_end, left_end, -1):
-            if (not reverse and vector[j] < vector[j-1]) or\
-               (    reverse and vector[j] > vector[j-1]):
-                swap(vector, j, j-1)
+            if (not reverse and lst[j] < lst[j-1]) or\
+               (    reverse and lst[j] > lst[j-1]):
+                swap(lst, j, j-1)
                 swapped = True
         left_end += 1
 
 
-#Sorts the elements in a list (vector) using Insertion sort, can sort in reverse order as well.
-def insertionSort(vector, reverse = False):
-    for i in range(1, len(vector)):
-        tmp = vector[i]
+#Sorts the elements in a list using Insertion sort, can sort in reverse order as well.
+def insertionSort(lst, reverse = False):
+    for i in range(1, len(lst)):
+        tmp = lst[i]
         j = i
-        while j > 0 and ((not reverse and tmp < vector[j-1]) or\
-                         (    reverse and tmp > vector[j-1])):
-            vector[j] = vector[j-1]
+        while j > 0 and ((not reverse and tmp < lst[j-1]) or\
+                         (    reverse and tmp > lst[j-1])):
+            lst[j] = lst[j-1]
             j -= 1
-        vector[j] = tmp
+        lst[j] = tmp
 
 
-#Sorts the elements in a list (vector) using Gnome sort, can sort in reverse order as well.
-def gnomeSort(vector, reverse = False):
-    v_length = len(vector)
+#Sorts the elements in a list using Gnome sort, can sort in reverse order as well.
+def gnomeSort(lst, reverse = False):
+    v_length = len(lst)
     i = 1
     while i < v_length:
-        if i == 0 or ((not reverse and vector[i-1] <= vector[i]) or\
-                      (    reverse and vector[i-1] >= vector[i])):
+        if i == 0 or ((not reverse and lst[i-1] <= lst[i]) or\
+                      (    reverse and lst[i-1] >= lst[i])):
             i += 1
         else:
-            swap(vector, i-1, i)
+            swap(lst, i-1, i)
             i -= 1
 
 
-#Merges the vector to be sorted and the temporary vector in the specified range (used for sorting using mergeSort()).
-def _merge(vector, tmp, left, right, right_end):
+#Merges the list to be sorted and the temporary list in the specified range (used for sorting using mergeSort()).
+def _merge(lst, tmp, left, right, right_end):
     left_end = right - 1
     tmpPos = left
     nb_elements = right_end - left + 1
 
     while left <= left_end and right <= right_end:
-        if vector[left] <= vector[right]:
-            tmp[tmpPos] = vector[left]
+        if lst[left] <= lst[right]:
+            tmp[tmpPos] = lst[left]
             tmpPos += 1
             left += 1
         else:
-            tmp[tmpPos] = vector[right]
+            tmp[tmpPos] = lst[right]
             tmpPos += 1
             right += 1
 
     while left <= left_end:
-        tmp[tmpPos] = vector[left]
+        tmp[tmpPos] = lst[left]
         tmpPos += 1
         left += 1
 
     while right <= right_end:
-        tmp[tmpPos] = vector[right]
+        tmp[tmpPos] = lst[right]
         tmpPos += 1
         right += 1
 
     for i in range(0, nb_elements):
-        vector[right_end] = tmp[right_end]
+        lst[right_end] = tmp[right_end]
         right_end -= 1
 
 
-#Recursive function used to sort sections of a vector (used by mergeSort()).
-def _mergeSort(vector, tmp, left, right):
+#Recursive function used to sort sections of a list (used by mergeSort()).
+def _mergeSort(lst, tmp, left, right):
     if right > left:
         center = (left + right) // 2
-        _mergeSort(vector, tmp, left, center)
-        _mergeSort(vector, tmp, center + 1, right)
-        _merge(vector, tmp, left, center + 1, right)
+        _mergeSort(lst, tmp, left, center)
+        _mergeSort(lst, tmp, center + 1, right)
+        _merge(lst, tmp, left, center + 1, right)
 
 
-#Sorts the elements in a list (vector) using Merge sort.
-def mergeSort(vector):
-    tmp = [0]*len(vector)
-    _mergeSort(vector, tmp, 0, len(vector)-1)
+#Sorts the elements in a list using Merge sort.
+def mergeSort(lst):
+    tmp = [0]*len(lst)
+    _mergeSort(lst, tmp, 0, len(lst)-1)
 
 
 
-#Partitions a list (vector) using the Lomuto partition scheme (used for Quicksort and Quickselect).
-def _partition(vector, left, right):
+#Partitions a list using the Lomuto partition scheme (used for Quicksort and Quickselect).
+def _partition(lst, left, right):
     pivotIndex = right #Note: We can change how we choose the pivot.
-    pivot = vector[pivotIndex] 
-    swap(vector, pivotIndex, right)
+    pivot = lst[pivotIndex] 
+    swap(lst, pivotIndex, right)
     i = left
     for j in range(left, right):
-        if vector[j] <= pivot:
-            swap(vector, i, j)
+        if lst[j] <= pivot:
+            swap(lst, i, j)
             i += 1
-    swap(vector, i, right)
+    swap(lst, i, right)
     return i
 
 
 #Recursive function used to find the element that would be at index k if the list was to be sorted (used by quickselect()).
-def _quickselect(vector, left, right, k):
+def _quickselect(lst, left, right, k):
      if left == right:
-         return vector[left]
-     pivotIndex = _partition(vector, left, right)
+         return lst[left]
+     pivotIndex = _partition(lst, left, right)
      if k == pivotIndex:
-         return vector[k]
+         return lst[k]
      elif k < pivotIndex:
-         return _quickselect(vector, left, pivotIndex-1, k)
+         return _quickselect(lst, left, pivotIndex-1, k)
      else:
-         return _quickselect(vector, pivotIndex+1, right, k)
+         return _quickselect(lst, pivotIndex+1, right, k)
 
 
-#Finds the element that would be at index k if the list (vector) was to be
+#Finds the element that would be at index k if the list was to be
 #sorted (without necessarily sorting the list) using Quickselect.
-def quickselect(vector, k):
-    return _quickselect(vector, 0, len(vector)-1, k)
+def quickselect(lst, k):
+    return _quickselect(lst, 0, len(lst)-1, k)
 
 
 #Recursive function used to sort sections of a list (used by quicksort()).
-def _quicksort(vector, left, right):
+def _quicksort(lst, left, right):
     if left < right:
-        pivot = _partition(vector, left, right)
-        _quicksort(vector, left, pivot-1)
-        _quicksort(vector, pivot+1, right)
+        pivot = _partition(lst, left, right)
+        _quicksort(lst, left, pivot-1)
+        _quicksort(lst, pivot+1, right)
 
 
-#Sorts the elements in a list (vector) using Quicksort (standard implementation).
-def quicksort(vector):
-    _quicksort(vector, 0, len(vector)-1)
+#Sorts the elements in a list using Quicksort (standard implementation).
+def quicksort(lst):
+    _quicksort(lst, 0, len(lst)-1)
 
 
 
-#Sorts the elements in a list (vector) using Counting sort (use only 
+#Sorts the elements in a list using Counting sort (use only 
 #when the range of values is smaller or equal than the number of values).
-def countingSort(vector):
-    min_val = min(vector)
-    max_val = max(vector)
+def countingSort(lst):
+    min_val = min(lst)
+    max_val = max(lst)
     size = max_val - min_val + 1
     counts = [0]*size
 
-    for i in range(0, len(vector)):
-        counts[vector[i]-min_val] += 1
+    for i in range(0, len(lst)):
+        counts[lst[i]-min_val] += 1
 
     cur_pos = 0
     for j in range(0, size):
         for k in range(0, counts[j]):
-            vector[cur_pos] = j + min_val
+            lst[cur_pos] = j + min_val
             cur_pos += 1
 
 
-#Sorts the elements, given as (key, val) tuples, in a list (vector) using Pigeonhole sort 
+#Sorts the elements, given as (key, val) tuples, in a list using Pigeonhole sort 
 #(use only when the range of values is smaller or equal than the number of values).
-def pingeonholeSort(vector):
-    min_val = min(vector, key=lambda e : e[0])
-    max_val = max(vector, key=lambda e : e[0])
+def pingeonholeSort(lst):
+    min_val = min(lst, key=lambda e : e[0])
+    max_val = max(lst, key=lambda e : e[0])
     size = max_val - min_val + 1
     indexed_vals = [[] for i in range(0, size)]
 
-    for i in range(0, len(vector)):
-        indexed_vals[vector[i][0]-min_val].append(vector[i][1])
+    for i in range(0, len(lst)):
+        indexed_vals[lst[i][0]-min_val].append(lst[i][1])
 
     cur_pos = 0
     for j in range(0, size):
         for k in range(0, len(indexed_vals[j])):
-            vector[cur_pos] = (j + min_val, indexed_vals[j][k])
+            lst[cur_pos] = (j + min_val, indexed_vals[j][k])
             cur_pos += 1
 
 
-#Returns a list containing the value of each (key,value) tuple in the vector
+#Returns a list containing the value of each (key,value) tuple in the list
 #(useful when using pingeonHoleSort() to keep only the values after sorting).
-def stripKeys(vector):
-    return [tup[1] for tup in vector]
+def stripKeys(lst):
+    return [tup[1] for tup in lst]
 
 
 
 #Test
-vector = createRandomVector(15, 1, 100)
-print(vector)
-improvedBubbleSort(vector)
-print(vector)
+lst = createRandomList(15, 1, 100)
+print(lst)
+oddEvenSort(lst)
+print(lst)
