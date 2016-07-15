accefcba6e13b15bf19a3a0b5457dbbfa72043e3
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2016-07-14 23:06:16 -0400

    Added binary heap.

--- sneklib/rp_data_structures.py
+++ sneklib/rp_data_structures.py
@@ -5,20 +5,16 @@
 
 #Binary search tree implemented using a zero-indexed dynamic array (list).
 class ArrayBST:
-    #Attributes
-    __elements = []
-    __cur_size = 0
-    __cur_max_size = 0
+    DEFAULT_SIZE = 10 #Default initial size of the underlying array.
+    DEFAULT_GROWTH_RATE = 2 #Default rate at which the underlying array's size increases when resized.
+
+    def __init__(self, initial_size = DEFAULT_SIZE, growth_rate = DEFAULT_GROWTH_RATE):
+        self.__cur_size = 0
+        self.__max_size = max(initial_size, 0)
+        self.__growth_rate = growth_rate if growth_rate > 1 else self.DEFAULT_GROWTH_RATE
+        self.__entries = [None for i in range(self.__max_size)]
 
 
-    #Constructor
-    def __init__(self, size = 0):
-        if size > 0:
-            self.__elements = [None] * size
-            self.__cur_max_size = size
-
-
-    #"Private" methods
     def __getParent(self, index):
         return (index - 1) // 2
 
@@ -28,15 +24,13 @@ class ArrayBST:
     def __getRightChild(self, root):
         return root * 2 + 2
 
-
     def __hasLeftChild(self, root):
         left_child_index = self.__getLeftChild(root)
-        return left_child_index < self.__cur_size and self.__elements[left_child_index] != None
+        return left_child_index < self.__cur_size and self.__entries[left_child_index] != None
 
     def __hasRightChild(self, root):
         right_child_index = self.__getRightChild(root)
-        return right_child_index < self.__cur_size and self.__elements[right_child_index] != None
-
+        return right_child_index < self.__cur_size and self.__entries[right_child_index] != None
 
     def __getLeftMost(self, root):
         pos = root
@@ -50,46 +44,43 @@ class ArrayBST:
             pos = self.__getRightChild(pos)
         return pos
 
-
     def __getIndex(self, val):
         pos = 0
-        while pos < self.__cur_max_size and self.__elements[pos] != None and self.__elements[pos] != val:
-            pos = self.__getLeftChild(pos) if val < self.__elements[pos] else self.__getRightChild(pos)
-        return None if pos >= self.__cur_max_size or self.__elements[pos] == None else pos
+        while pos < self.__max_size and self.__entries[pos] != None and self.__entries[pos] != val:
+            pos = self.__getLeftChild(pos) if val < self.__entries[pos] else self.__getRightChild(pos)
+        return None if pos >= self.__max_size or self.__entries[pos] == None else pos
+
+    def __enlarge(self):
+        new_size = self.__max_size * self.__growth_rate + 1
+        delta =  new_size - self.__cur_size #+1 is to insure the size doesn't get stuck at 0.
+        self.__entries.extend([None for i in range(delta)])
+        self.__max_size = new_size
+
+    def __getSortedArray(self, root, array):
+        if self.__cur_size > 0 and root < self.__max_size and self.__entries[root] != None:
+            self.__getSortedArray(self.__getLeftChild(root), array)
+            array.append(self.__entries[root])
+            self.__getSortedArray(self.__getRightChild(root), array)
+
+    def __getReverseArray(self, root, array):
+        if self.__cur_size > 0 and root < self.__max_size and self.__entries[root] != None:
+            self.__getReverseArray(self.__getRightChild(root), array)
+            array.append(self.__entries[root])
+            self.__getReverseArray(self.__getLeftChild(root), array)
+
+    def __getPreOrderArray(self, root, array):
+        if self.__cur_size > 0 and root < self.__max_size and self.__entries[root] != None:
+            array.append(self.__entries[root])
+            self.__getPreOrderArray(self.__getLeftChild(root), array)
+            self.__getPreOrderArray(self.__getRightChild(root), array)
+
+    def __getPostOrderArray(self, root, array):
+        if self.__cur_size > 0 and root < self.__max_size and self.__entries[root] != None:    
+            self.__getPostOrderArray(self.__getLeftChild(root), array)
+            self.__getPostOrderArray(self.__getRightChild(root), array)
+            array.append(self.__entries[root])
 
 
-    def __enlarge(self, new_size):
-        if new_size > self.__cur_max_size:
-            self.__elements.extend([None] * (new_size - self.__cur_max_size))
-            self.__cur_max_size = new_size
-
-
-    def __printInOrder(self, root):
-        if self.__cur_size > 0 and root < self.__cur_max_size and self.__elements[root] != None:
-            self.__printInOrder(self.__getLeftChild(root))
-            print(self.__elements[root])
-            self.__printInOrder(self.__getRightChild(root))
-
-    def __printReverseOrder(self, root):
-        if self.__cur_size > 0 and root < self.__cur_max_size and self.__elements[root] != None:
-            self.__printReverseOrder(self.__getRightChild(root))
-            print(self.__elements[root])
-            self.__printReverseOrder(self.__getLeftChild(root))
-
-    def __printPreOrder(self, root):
-        if self.__cur_size > 0 and root < self.__cur_max_size and self.__elements[root] != None:
-            print(self.__elements[root])
-            self.__printPreOrder(self.__getLeftChild(root))
-            self.__printPreOrder(self.__getRightChild(root))
-
-    def __printPostOrder(self, root):
-        if self.__cur_size > 0 and root < self.__cur_max_size and self.__elements[root] != None:    
-            self.__printPostOrder(self.__getLeftChild(root))
-            self.__printPostOrder(self.__getRightChild(root))
-            print(self.__elements[root])
-
-
-    #Public methods
     def size(self):
         return self.__cur_size
 
@@ -99,68 +90,178 @@ class ArrayBST:
     def contains(self, val):
         return self.__getIndex(val) != None
 
-
     def insert(self, val):
-        if self.__cur_size == self.__cur_max_size:
-            self.__enlarge(2 * self.__cur_max_size + 1)
+        if self.__cur_size == self.__max_size:
+            self.__enlarge()
 
         pos = 0
-        while pos < self.__cur_max_size and self.__elements[pos] != None:
-            if self.__elements[pos] == val:
+        while pos < self.__max_size and self.__entries[pos] != None:
+            if self.__entries[pos] == val:
                 return #No duplicates
-            pos = self.__getLeftChild(pos) if val < self.__elements[pos] else  self.__getRightChild(pos)
+            pos = self.__getLeftChild(pos) if val < self.__entries[pos] else  self.__getRightChild(pos)
 
-        if pos >= self.__cur_max_size:
-            self.__enlarge(2 * self.__cur_max_size + 1)
+        if pos >= self.__max_size:
+            self.__enlarge()
 
-        self.__elements[pos] = val
+        self.__entries[pos] = val
         self.__cur_size += 1
 
+    def insertArray(self, array):
+        for a in array:
+            self.insert(a)
 
     def remove(self, val):
         index = self.__getIndex(val)
         if index != None:
             if not self.__hasLeftChild(index) and not self.__hasRightChild(index):
-                self.__elements[index] = None
+                self.__entries[index] = None
             elif self.__hasLeftChild(index) and not self.__hasRightChild(index):
                 left_child_index = self.__getLeftChild(index)
-                self.__elements[index] = self.__elements[left_child_index]
-                self.__elements[left_child_index] = None
+                self.__entries[index] = self.__entries[left_child_index]
+                self.__entries[left_child_index] = None
             elif self.__hasRightChild(index) and not self.__hasLeftChild(index):
                 right_child_index = self.__getRightChild(index)
-                self.__elements[index] = self.__elements[right_child_index]
-                self.__elements[right_child_index] = None
+                self.__entries[index] = self.__entries[right_child_index]
+                self.__entries[right_child_index] = None
             else:
                 new_root_index = self.__getLeftMost(self.__getRightChild(index))
-                self.__elements[index] = self.__elements[new_root_index]
-                self.__elements[new_root_index] = None
+                self.__entries[index] = self.__entries[new_root_index]
+                self.__entries[new_root_index] = None
             self.__cur_size -= 1
 
-
     def getMax(self):
-        return self.__elements[self.__getRightMost(0)] if not self.isEmpty() else None
+        return self.__entries[self.__getRightMost(0)] if not self.isEmpty() else None
 
     def getMin(self):
-        return self.__elements[self.__getLeftMost(0)] if not self.isEmpty() else None
+        return self.__entries[self.__getLeftMost(0)] if not self.isEmpty() else None
+
+    def getSortedArray(self):
+        array = []
+        self.__getSortedArray(0, array)
+        return array
+
+    def getReverseArray(self, array):
+        array = []
+        self.__getReverseArray(0)
+        return array
+
+    def getPreOrderArray(self, array):
+        array = []
+        self.__getPreOrderArray(0)
+        return array
+
+    def getPostOrderArray(self, array):
+        array = []
+        self.__getPostOrderArray(0)
+        return array
 
 
-    def printInOrder(self):
-        self.__printInOrder(0)
 
-    def printReverseOrder(self):
-        self.__printReverseOrder(0)
+#Class used to represent an entry in the binary heap.
+class HeapEntry:
+    def __init__(self, value, priority):
+        self.value = value
+        self.priority = priority
 
-    def printPreOrder(self):
-        self.__printPreOrder(0)
 
-    def printPostOrder(self):
-        self.__printPostOrder(0)
+#Binary heap implemeneted using a dynamic array (list).
+class BinaryHeap:
+    DEFAULT_SIZE = 10 #Default initial size of the underlying array.
+    DEFAULT_GROWTH_RATE = 2 #Default rate at which the underlying array's size increases when resized.
+
+    def __init__(self, initial_size = DEFAULT_SIZE, growth_rate = DEFAULT_GROWTH_RATE):
+        self.__cur_size = 0
+        self.__max_size = max(initial_size + 1, 1)
+        self.__growth_rate = growth_rate if growth_rate > 1 else self.DEFAULT_GROWTH_RATE
+        self.__entries = [None for i in range(self.__max_size)]
+
+
+    #Resizes the underlying array according to the set growth rate.
+    def __enlarge(self):
+        new_size = self.__max_size * self.__growth_rate + 1
+        delta =  new_size - self.__cur_size #+1 is to insure the size doesn't get stuck at 0.
+        self.__entries.extend([None for i in range(delta)])
+        self.__max_size = new_size
+
+    #Percolates an entry down to it's right position.
+    def __percolateDown(self, hole):
+        tmp = HeapEntry(self.__entries[hole].value, self.__entries[hole].priority)
+        while hole * 2 <= self.__cur_size:
+            child = hole * 2
+            if child != self.__cur_size and self.__entries[child+1].priority < self.__entries[child].priority:
+                child += 1
+            if self.__entries[child].priority < tmp.priority:
+                self.__entries[hole] = self.__entries[child]
+            else:
+                break
+            hole = child
+        self.__entries[hole] = tmp
+
+    #Percolates an entry up to it's right position.
+    def __percolateUp(self, hole, entry):
+        while hole > 1 and entry.priority < self.__entries[hole//2].priority:
+            self.__entries[hole] = HeapEntry(self.__entries[hole//2].value, self.__entries[hole//2].priority)
+            hole //= 2
+        self.__entries[hole] = entry
+
+    #Returns the number of entries in the heap.
+    def size(self):
+        return self.__cur_size
+
+    #Returns a boolean indicating if the heap is empty.
+    def isEmpty(self):
+        return self.size() == 0
+
+    #Returns the value at the top of the heap without removing it.
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return entries[1].value
+
+    #Returns the value at the top of the heap and removes it.
+    def pop(self):
+        if self.isEmpty():
+            return None
+        val = self.__entries[1].value
+        self.__entries[1] = self.__entries[self.__cur_size]
+        self.__cur_size -= 1
+        self.__percolateDown(1)
+        return val
+
+    #Inserts an new entry in the heap (the value is considered as being the priority as well).
+    def insert(self, value):
+        self.insertPair(value, value)
+
+    #Inserts an new entry in the heap.
+    def insertPair(self, value, priority):
+        if self.__cur_size == self.__max_size:
+            self.__enlarge()
+        hole = self.__cur_size + 1
+        self.__cur_size += 1
+        new_entry = HeapEntry(value, priority)
+        self.__percolateUp(hole, new_entry)
+
+    #Inserts every element of the array in the heap (the value is considered as being the priority as well).
+    def insertArray(self, array):
+        for e in array:
+            self.insert(e)
+
+    #Inserts every element of the array given as (value, priority) tuples/lists in the heap.
+    def insertPairArray(self, array):
+        for e in array:
+            self.insertPair(e[0], e[1])
+
 
 
 
 #Test
-bst = ArrayBST()
 array = [3,34,6,8,2,3,1,9,67]
-for a in array:
-    bst.insert(a)
-bst.printInOrder()
+
+#bst = ArrayBST()
+#bst.insertArray(array)
+#print(bst.getSortedArray())
+
+heap = BinaryHeap()
+heap.insertArray(array)
+while not heap.isEmpty():
+    print(heap.pop())
